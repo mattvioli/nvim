d6873b99694f5c8cd7ff3e5e06d5d42cb50aaa4b
Violi-oua <matthew.violi@open.edu.au>
2024-07-10 12:42:06 +1000

    fix mode typo

--- lua/plugins/refactoring.lua
+++ lua/plugins/refactoring.lua
@@ -5,7 +5,7 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 	},
 	keys = {
-		{ "<leader>re", ":Refactor extract ", mode = "z" },
+		{ "<leader>re", ":Refactor extract ", mode = "x" },
 		{ "<leader>rv", ":Refactor extract_var ", mode = "x" },
 		{ "<leader>rf", ":Refactor extract_to_file ", mode = "x" },
 		{ "<leader>ri", ":Refactor inline_var", mode = { "n", "x" } },

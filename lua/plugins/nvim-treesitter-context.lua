f672b35c3871b8e02642551c6a1a2d418b612211
mattvioli <mattvioli@gmail.com>
2024-09-30 08:14:50 +1000

    update treesitte context

--- lua/plugins/nvim-treesitter-context.lua
+++ lua/plugins/nvim-treesitter-context.lua
@@ -1,6 +1,5 @@
 return {
-  "nvim-treesitter/nvim-treesitter-context",
-  event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-  enabled = true,
-  opts = { mode = "cursor" }
+	"nvim-treesitter/nvim-treesitter-context",
+	event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+	opts = { mode = "cursor", enable = true, multiline_threshold = 1 },
 }
